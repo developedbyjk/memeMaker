5b1983399c9bf92a515777ebff5411ae4edf3d87
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-07-10 22:32:36 +0530

    main

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -79,7 +79,7 @@ export default function Meme() {
                     className="form--button"
                     onClick={getMemeImage}
                 >
-                    Get a new meme image 🖼
+                    Get a new meme image
                 </button>
                 <button
                     className="form--button"
